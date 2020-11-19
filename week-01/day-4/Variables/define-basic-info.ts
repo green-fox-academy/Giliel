d2e5349d3e0ff554d59f6198289ab10a30d995d7
Gabriella Merényi <gabi.merenyi@gmail.com>
2020-11-19 17:14:14 +0100

    add 3 new exercises

---
+++ week-01/day-4/Variables/define-basic-info.ts
@@ -0,0 +1,16 @@
+'use strict';
+
+let yourName: string = 'Gabriella Merényi';
+let age: number = 43;
+let height: number = 1.63;
+let married: boolean = false;
+
+console.log('Name: ' + yourName);
+console.log('Age: ' + age);
+console.log('Height: ' + height);
+
+if (married) {
+  console.log('Married');
+} else {
+  console.log('Not married');
+}
