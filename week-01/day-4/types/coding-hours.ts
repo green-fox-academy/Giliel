923ce7b0d5315857677b2098bf424797ae21be56
Gabriella Merényi <gabi.merenyi@gmail.com>
2020-11-19 15:34:21 +0100

    day-4 dir: 3 files moved to subdir, add types dir and files

---
+++ week-01/day-4/types/coding-hours.ts
@@ -0,0 +1,4 @@
+'use strict';
+
+console.log('Hours is spent with coding: ' + (6 * 17 * 5));
+console.log('The percentage of coding hours: ' + (17 * 168 / 52));
---
+++ week-01/day-4/types/introduce-yourself.ts
@@ -0,0 +1,5 @@
+'use strict';
+
+console.log('Gabriella Merényi');
+console.log(43);
+console.log(1.63);
