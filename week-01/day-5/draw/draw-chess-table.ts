f4fd0e72dfe5943f9240cfabd9c37cda96ca2ee3
Gabriella Merényi <gabi.merenyi@gmail.com>
2020-11-20 23:10:33 +0100

    modify draw-chess-table

--- week-01/day-5/draw/draw-chess-table.ts
+++ week-01/day-5/draw/draw-chess-table.ts
@@ -1,13 +1,13 @@
 'use strict';
 
-let penSpace: string = '% ';
-let spacePen: string = ' %';
+for (let i: number = 0; i < 4; i++) {
+  let penSpace: string = '% ';
+  let spacePen: string = ' %';
 
-for (let i: number = 0; i < 3; i++) {
-  penSpace += '% ';
-  spacePen += ' %';
-}
-for (let j: number = 0; j < 4; j++) {
+  for (let j: number = 0; j < 3; j++) {
+    penSpace += '% ';
+    spacePen += ' %';
+  }
   console.log(penSpace);
   console.log(spacePen);
 }
