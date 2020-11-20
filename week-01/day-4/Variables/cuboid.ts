d6eb524e7c11ce879847cac4cfa9cce1f348dc5a
Gabriella Merényi <gabi.merenyi@gmail.com>
2020-11-20 10:28:43 +0100

    change cuboid

--- week-01/day-4/Variables/cuboid.ts
+++ week-01/day-4/Variables/cuboid.ts
@@ -1,8 +1,10 @@
 'use strict';
 
-let a: number = 10;
-let surfaceArea: number = (6 * (a ** 2));
-let volume: number = (a ** 3);
+let a: number = 10.0;
+let b: number = 10.0;
+let c: number = 10.0;
+let surfaceArea: number = (2 * ((a ** 2) + (b ** 2) + (c ** 2)));
+let volume: number = (a * b * c);
 
 console.log('Surface Area: ' + surfaceArea);
 console.log('Volume: ' + volume);
