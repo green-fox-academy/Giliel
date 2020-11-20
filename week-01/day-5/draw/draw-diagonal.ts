fcd00d096e7f66dab77279d80940a47ab0b88874
Gabriella Merényi <gabi.merenyi@gmail.com>
2020-11-20 22:20:54 +0100

    add draw-diagonal

---
+++ week-01/day-5/draw/draw-diagonal.ts
@@ -0,0 +1,22 @@
+'use strict'
+
+let lineCount: number = 6;
+let space1: string = '';
+
+for (let i: number = 0; i < lineCount; i++) {
+  let pen: string = '%';
+
+  if ((i == 0) || (i == (lineCount - 1))) {
+    for (let j: number = 0; j < (lineCount - 1); j++) {
+      pen += '%';
+    }
+    console.log(pen);
+  } else {
+    let space2: string = '';
+    for (let j: number = (lineCount - 2 - i); j > 0; j--) {
+      space2 += ' '
+    }
+    console.log(pen + space1 + pen + space2 + pen);
+    space1 += ' ';
+  }
+}
