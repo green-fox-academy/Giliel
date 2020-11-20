ba60b308488d7577967f86c06461e1457b9fbf18
Gabriella Merényi <gabi.merenyi@gmail.com>
2020-11-20 10:03:06 +0100

    add cuboid

---
+++ week-01/day-4/Variables/cuboid.ts
@@ -0,0 +1,8 @@
+'use strict';
+
+let a: number = 10;
+let surfaceArea: number = (6 * (a ** 2));
+let volume: number = (a ** 3);
+
+console.log('Surface Area: ' + surfaceArea);
+console.log('Volume: ' + volume);
