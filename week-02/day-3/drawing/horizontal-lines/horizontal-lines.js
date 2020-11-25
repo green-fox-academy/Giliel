d952d60a1fe9d940941718f4f38a5ae2636694ba
Gabriella Merényi <gabi.merenyi@gmail.com>
2020-11-25 16:46:00 +0100

    add drawing horizontal-lines

---
+++ week-02/day-3/drawing/horizontal-lines/horizontal-lines.js
@@ -0,0 +1,23 @@
+'use strict';
+
+// Boilerplate
+const canvas = document.querySelector('.canvas');
+const ctx = canvas.getContext('2d');
+// DO NOT TOUCH THE CODE ABOVE THIS LINE
+
+// Create a function that draws a single line and takes 2 parameters:
+// the x and y coordinates of the line's starting point
+// and draws a 50 long horizontal line from that point.
+// Draw at least 3 lines with that function using a loop.
+
+function drawLine(startX, startY) {
+  ctx.beginPath();
+  ctx.moveTo(startX, startY);
+  ctx.lineTo(startX + 50, startY);
+  ctx.stroke();
+}
+
+for (let i = 0; i < 3; i++) {
+
+  drawLine(i * 80, 60);
+}
