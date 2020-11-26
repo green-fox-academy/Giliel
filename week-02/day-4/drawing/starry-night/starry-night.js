0224e58c5306852c2c821e016042c4bdc93ad554
Gabriella Merényi <gabi.merenyi@gmail.com>
2020-11-26 15:19:56 +0100

    add drawing starry-night

---
+++ week-02/day-4/drawing/starry-night/starry-night.js
@@ -0,0 +1,28 @@
+'use strict';
+
+// Boilerplate
+const canvas = document.querySelector('.canvas');
+const ctx = canvas.getContext('2d');
+// DO NOT TOUCH THE CODE ABOVE THIS LINE
+
+// Draw the night sky:
+//  - The background should be black
+//  - The stars should be small squares
+//  - The stars should have random positions on the canvas
+//  - The stars should have random color (some shade of grey)
+function rect(startX, startY, width, height, color) {
+
+  ctx.fillStyle = color;
+  ctx.fillRect(startX, startY, width, height);
+}
+
+function stars(starNum) {
+
+  for (let i = 0; i < starNum; i++) {
+
+    rect((590 * Math.random()), (390 * Math.random()), 10, 10, (`rgba(255, 255, 255, ${0.4 + Math.random()})`));
+  }
+}
+
+rect(0, 0, 600, 400, 'black');
+stars(12);
