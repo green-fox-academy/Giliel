12223e3f3cfee9b26b148ee8c3ede8d260f4932d
Gabriella Merényi <gabi.merenyi@gmail.com>
2020-12-03 23:17:52 +0100

    add counter

---
+++ week-03/day-3/encapsulationAndConstructor/counter/counter.ts
@@ -0,0 +1,24 @@
+'use strict';
+
+class Counter {
+  private _number: number;
+  private _initialNumber: number;
+
+  constructor(number?: number) {
+    this._number = number ?? 0;
+    this._initialNumber = number ?? 0;
+  }
+
+  public add(number?: number): void {
+   this._number += number ?? 1;
+  }
+
+  public get(): string {
+    return `${this._number}`;
+  }
+  public reset(): void {
+    this._number = this._initialNumber;
+  }
+}
+
+export { Counter };
