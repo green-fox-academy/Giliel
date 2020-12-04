1c805179fb054e65e71877f4bed325ad33079420
Gabriella Merényi <gabi.merenyi@gmail.com>
2020-12-04 16:26:46 +0100

    add bunnies2

--- week-03/day-5/recursion/bunnies2.ts
+++ week-03/day-5/recursion/bunnies2.ts
@@ -4,7 +4,7 @@ function earCounter(bunnies: number): number {
   if (bunnies === 0) {
     return 0;
   }
-  return 2 + earCounter(bunnies - 1);
+  return (3 - (bunnies % 2)) + earCounter(bunnies - 1);
 }
 
-console.log(earCounter(8));
+console.log(earCounter(22));
