ecfcaa1ff72729bce9efac2595cba90404e1aecd
Gabriella Merényi <gabi.merenyi@gmail.com>
2020-12-09 15:05:20 +0100

    add tape and apples

---
+++ week-04/day-3/apples/apples.test.ts
@@ -0,0 +1,14 @@
+'use strict';
+
+import * as test from 'tape';
+import { Apple } from './apples';
+
+test('get string', t => {
+  const fruit = new Apple();
+
+  let actual = fruit.getApple();
+  let expected = 'apples';
+
+  t.equal(actual, expected);
+  t.end();
+})
