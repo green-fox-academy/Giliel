c8cc98e46e3024646e3da62e695c2102336d39be
Gabriella Merényi <gabi.merenyi@gmail.com>
2020-12-17 15:13:22 +0100

    replace  sum-all method, add matrix

--- week-02/day-1/arrays/sum-all.ts
+++ week-02/day-1/arrays/sum-all.ts
@@ -5,10 +5,7 @@
 // -  Log the sum of the elements of `numbers` to the console
 
 let numbers: number[] = [3, 4, 5, 6, 7];
-let sum: number = 0;
 
-numbers.forEach(function(value: number): number {
-  return sum += value;
-});
-
-console.log(sum);
+console.log(numbers.reduce(function(numCollector: number, currentNumber: number): number {
+  return numCollector + currentNumber;
+}));
