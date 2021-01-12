7ad05bb95211009671640e216c492ec42541b234
Gabriella Merényi <d:\Netrol\Level>
2021-01-12 19:44:49 +0100

    add exercise frontend

---
+++ week-07/day-2/frontend/app.js
@@ -0,0 +1,22 @@
+'use strict';
+
+const express = require('express');
+const path = require('path');
+
+const app = express();
+
+app.use(express.static('assets'));
+
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, 'index.html'));
+});
+
+
+
+
+
+
+
+
+
+app.listen(3000);
