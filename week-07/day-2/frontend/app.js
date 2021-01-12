7b30ca11a50f1f9d7296c0493be87d772f5a86eb
Gabriella Merényi <d:\Netrol\Level>
2021-01-12 20:27:39 +0100

    add endpont doubling

--- week-07/day-2/frontend/app.js
+++ week-07/day-2/frontend/app.js
@@ -11,6 +11,17 @@ app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, 'index.html'));
 });
 
+app.get('/doubling', (req, res) => {
+  let baseNum = req.query.input;
+
+  if (baseNum === undefined) {
+    res.json({ error: 'Please provide an input!' });
+  } else {
+    let doubledNum = baseNum * 2;
+    res.json({ received: Number(baseNum), result: doubledNum });
+  }
+});
+
 
 
 
