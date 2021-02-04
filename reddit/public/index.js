f74097dc0501b9665e37c3156e6605747df9f185
Gabriella Merényi <d:\Netrol\Level>
2021-02-04 16:18:25 +0100

    add count and show elapsed time in different timeunits

--- reddit/public/index.js
+++ reddit/public/index.js
@@ -26,6 +26,11 @@ function getPosts() {
 
 function loadNewPost(postDatas) {
   let elapsedTime = countElapsedTime(postDatas.timestamp);
+  let username = 'Anonymus';
+
+  if (postDatas.username) {
+    username = postDatas.username;
+  }
 
   const postCard = document.createElement('div');
   postCard.setAttribute('id', postDatas.post_id);
@@ -40,7 +45,7 @@ function loadNewPost(postDatas) {
   upvote.setAttribute('class', 'upvote');
   postVote.appendChild(upvote);
 
-  const postScore = document.createElement('div');
+  const postScore = document.createElement('p');
   postScore.setAttribute('class', 'postScore');
   postScore.innerHTML = postDatas.score;
   postVote.appendChild(postScore);
@@ -61,7 +66,7 @@ function loadNewPost(postDatas) {
 
   const postSubmitted = document.createElement('p');
   postSubmitted.setAttribute('class', 'postSubmitted');
-  postSubmitted.innerHTML = `submitted ${elapsedTime} minutes ago`;
+  postSubmitted.innerHTML = `submitted ${elapsedTime} ago by ${username}`;
   post.appendChild(postSubmitted);
 
   const postLinkModify = document.createElement('a');
@@ -83,7 +88,28 @@ function countElapsedTime(timestamp) {
 
   let actualDateValue = Date.now();
 
-  let elapsedTime = Math.floor((actualDateValue - postDateValue) / 1000 / 60);
+  let minutes = Math.floor((actualDateValue - postDateValue) / 1000 / 60);
 
-  return `${elapsedTime}`;
+  let years = Math.floor(minutes / 60 / 24 / 365);
+  let months = Math.floor(minutes / 60 / 24 / 30);
+  let weeks = Math.floor(minutes / 60 / 24 / 7);
+  let days = Math.floor(minutes / 60 / 24);
+  let hours = Math.floor(minutes / 60);
+
+  let elapsedTime = '';
+
+  if (years > 0) {
+    elapsedTime = `${years} ${(years === 1) ? 'year' : 'years'}`;
+  } else if (months > 0) {
+    elapsedTime = `${months} ${(months === 1) ? 'month' : 'months'}`;
+  } else if (weeks > 0) {
+    elapsedTime = `${weeks} ${(weeks === 1) ? 'week' : 'weeks'}`;
+  } else if (days > 0) {
+    elapsedTime = `${days} ${(days === 1) ? 'day' : 'days'}`;
+  } else if (hours > 0) {
+    elapsedTime = `${hours} ${(hours === 1) ? 'hour' : 'hours'}`;
+  } else {
+    elapsedTime = `${minutes} ${(minutes < 2) ? 'minute' : 'minutes'}`;
+  }
+  return elapsedTime;
 }
